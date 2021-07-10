8f5656243c9155a29618c5dc16b20c2879c11c44
DajanaM22 <dajana.milosevic@gmail.com>
2021-07-10 15:19:40 +0200

    changes with stopwatch

--- VezbanjeGita/Program.cs
+++ VezbanjeGita/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace VezbanjeGita
 {
@@ -11,6 +12,30 @@ namespace VezbanjeGita
             Console.WriteLine("Hello World again!");
             Console.WriteLine("Some changes from serveerr!!");
             Console.WriteLine("Some changes from locaaal!");
+
+            double [] niz = {1, 2, 3, 5, 1, 9 , 3, 3, 5, 1, 9, 3, 5, 1, 9, 3, 5, 1, 9, 3, 5, 1, 9, 3, 5, 1, 9, 3, 5, 1, 9};
+            double sum= 0;
+            double sum2= 0;
+            var watch = Stopwatch.StartNew();
+            for(int i=0;i<niz.Length;i++)
+            {
+                sum+= niz[i];
+
+            }
+            watch.Stop();
+
+            var watch2 = Stopwatch.StartNew();
+
+            foreach(var el in niz)
+            {
+                sum2+=el;
+            }
+            watch2.Stop();
+
+
+
+            Console.WriteLine("Elapsed={0}, {1}", watch.Elapsed, sum);
+            Console.WriteLine("Elapsed={0}, {1}", watch2.Elapsed, sum2);
         }
     }
 }
